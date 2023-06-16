2940ab044f86fe1b40565a96b67e7cce0b42aecc
ifs20001-itdel <ifs20001@students.del.ac.id>
2023-06-16 11:35:32 +0700

    second

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,6 @@ const Layout = ({children}) => (
     <>
         <Head>
             <title>Note App</title>
-            <script src="https://cdn.tailwindcss.com"></script>
         </Head>
         <Navbar />
         {children}
