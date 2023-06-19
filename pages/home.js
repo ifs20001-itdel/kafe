c76f36943a5cf6b93f3f7af51fb65dd6b1853a0a
ifs20001-itdel <ifs20001@students.del.ac.id>
2023-06-19 13:48:07 +0700

    push

--- pages/home.js
+++ pages/home.js
@@ -142,6 +142,21 @@ const Home = () => (
         </div>
     </div>
     </div>
+    {/* Location */}
+    <div className="text-gray-600 body-font">
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Location</h1>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img className="object-cover object-center rounded-xl" alt="hero" src="/lokasi.png" />
+                </div>
+                <p className="mb-8 leading-relaxed mt-5">KOMPLEK, Jl. Classic II Jl. Abdul Hakim, Tj. Sari, Kec. Medan Selayang, Kota Medan, Sumatera Utara 20131</p>
+                <p className="mb-8 leading-relaxed">Open at 09.00 AM - 11.00 PM</p>
+            </div>
+
+            </div>
+        </div>
+
 
     </main>
 )
