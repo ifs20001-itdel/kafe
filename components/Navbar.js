bffa4288c4a335a32a6661dc5225eb52f01f3016
ifs20001-itdel <ifs20001@students.del.ac.id>
2023-06-19 15:39:33 +0700

    dashboard

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,42 +1,68 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from 'react';
 
-const Navbar = () => (
-    // <nav className="navbar">
-    //     <Link href="/">
-    //         <span className="navbar-brand">Note App</span>
-    //     </Link>
-    //     <Link href="/new">
-    //         <span className="create">Create note</span>
-    //     </Link>
-    // </nav>
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
     <>
-        <nav class="text-gray-600 body-font mb-10">
-            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <span class="ml-3 text-xl">
-                <Image src="/kefi.png" width={50} height={50}></Image>
-                </span>
-            </a>
-            <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                <Link class="mr-5 hover:text-gray-900" href="/home">Home Page</Link>
-                <Link class="mr-5 hover:text-gray-900" href="/">Menu</Link>
-                <Link class="mr-5 hover:text-gray-900" href="/index_promo">Promo</Link>
-                <Link class="mr-5 hover:text-gray-900" href="/contact">Contact</Link>
-                <Link class="mr-5 hover:text-gray-900" href="/new">CREATE</Link>
-            </nav>
-            <Link href="/autentikasi">
-                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                Akun
-                </button>
-            </Link>
+      <nav className="text-gray-600 body-font mb-10">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row">
+          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-xl">
+              <Image src="/kefi.png" width={50} height={50}></Image>
+            </span>
+          </div>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <div className="mr-5 md:hidden">
+            <button className="focus:outline-none"
+                onClick={toggleMobileMenu}>
+                <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+                <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden group-focus:translate-x-1.5">
+                    <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:rotate-[42deg] group-focus:w-2/3 delay-150"></div>
+                    <div class="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-x-10"></div>
+                    <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:-rotate-[42deg] group-focus:w-2/3 delay-150"></div>
+                </div>
+                </div>
+            </button>
             </div>
-        </nav>
+
+            <div
+              className={`${
+                isMobileMenuOpen ? '' : 'hidden'
+              } md:flex flex-wrap items-center`}
+            >
+              <Link href="/home" className="mr-5 hover:text-gray-900">
+                <p>Home Page</p>
+              </Link>
+              <Link href="/" className="mr-5 hover:text-gray-900">
+                <p>Menu</p>
+              </Link>
+              <Link href="/index_promo" className="mr-5 hover:text-gray-900">
+                <p>Promo</p>
+              </Link>
+              <Link href="/contact" className="mr-5 hover:text-gray-900">
+                <p>Contact</p>
+              </Link>
+              <Link href="/new" className="mr-5 hover:text-gray-900">
+                <p>CREATE</p>
+              </Link>
+            </div>
+          </nav>
+          <Link href="/autentikasi">
+            <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+              Akun
+            </button>
+          </Link>
+        </div>
+      </nav>
     </>
-
-
-
-
-);
+  );
+};
 
 export default Navbar;
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { Button } from "semantic-ui-react";
 
-const Home = () => (
+const Home = () => {
+    const openMaps = () => {
+      const width = 600;
+      const height = 600;
+      const left = window.screen.width / 2 - width / 2;
+      const top = window.screen.height / 2 - height / 2;
+      const options = `width=${width},height=${height},top=${top},left=${left}`;
+  
+      window.open("https://goo.gl/maps/C5y8Qym7WoCpMKeB8", "Google Maps", options);
+    };
+return (
     <main>
         <div style={{ width: `100%`, height: `100%`, position: `relative` }}>
                 <img src="/background.png" style={{ objectFit: `cover` }} />
@@ -17,11 +28,11 @@ const Home = () => (
             <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Our Story</h1>
-                <p className="mb-8 leading-relaxed">Kefi Coffee And Space is a restaurant located in Medan. This restaurant serves various drinks and rice menus at affordable prices.</p>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                 <img className="object-cover object-center rounded" alt="hero" src="/story.png" />
             </div>
+            <p className="mt-8 leading-relaxed">Kefi Coffe And Space merupakan sebuah tempat makan yang berada di Medan. Rumah makan ini menyajikan berbagai menu minuman & aneka nasi yang dibanderol dengan harga yang murah dan bersahabat dengan kantong Anda.</p>
             </div>
         </div>
 
@@ -104,61 +115,76 @@ const Home = () => (
     </div>
     </div>
 
-        {/* PROMO */}
-        <div class="text-gray-600 body-font">
+        {/* MINUMAN */}
+        <div class="text-gray-600 body-font mb-10">
     <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">PROMO</h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven`t heard of them.</p>
-        </div>
-        <div class="flex flex-wrap -m-4">
-        <div class="p-4 lg:w-1/4 md:w-1/2">
-            <div class="h-full flex flex-col items-center text-center">
-            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="minuman.png" />
-            <div class="w-full">
-                <h2 class="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
-                <h3 class="text-gray-500 mb-3">UI Developer</h3>
-                <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-                <span class="inline-flex">
-                <div class="text-gray-500">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                </div>
-                <div class="ml-2 text-gray-500">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                </div>
-                <div class="ml-2 text-gray-500">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                </div>
-                </span>
-            </div>
-            </div>
+        <div class="text-center mb-10">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Promo</h1>
+        <div class="flex mt-6 justify-center">
+            <div class="w-16 h-1 rounded-full bg-yellow-500 inline-flex"></div>
         </div>
         </div>
+
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+        <div class="text-gray-600 body-font">
+
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/minuman1.JPG" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="minuman2.JPG" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="minuman3.JPG" class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     </div>
     </div>
+    </div>
+    </div>
+
+        {/* TESTIMONI */}
+        <div className="text-gray-600 body-font">
+            {/* <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Our Story</h1>
+                <p className="mb-8 leading-relaxed">Kefi Coffee And Space is a restaurant located in Medan. This restaurant serves various drinks and rice menus at affordable prices.</p>
+            </div> */}
+                <div className="w-full">
+                    <img className="w-full" alt="testimoni" src="/Testimonials.png" />
+                </div>
+            {/* </div> */}
+        </div>
+
     {/* Location */}
     <div className="text-gray-600 body-font">
-            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Location</h1>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img className="object-cover object-center rounded-xl" alt="hero" src="/lokasi.png" />
-                </div>
-                <p className="mb-8 leading-relaxed mt-5">KOMPLEK, Jl. Classic II Jl. Abdul Hakim, Tj. Sari, Kec. Medan Selayang, Kota Medan, Sumatera Utara 20131</p>
-                <p className="mb-8 leading-relaxed">Open at 09.00 AM - 11.00 PM</p>
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Location</h1>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img className="object-cover object-center rounded-xl" alt="hero" src="/lokasi.png" />
             </div>
-
-            </div>
+            <p className="mb-8 leading-relaxed mt-5">KOMPLEK, Jl. Classic II Jl. Abdul Hakim, Tj. Sari, Kec. Medan Selayang, Kota Medan, Sumatera Utara 20131</p>
+            <p className="mb-8 leading-relaxed">Open at 09.00 AM - 11.00 PM</p>
+          </div>
+          <Button style={{ backgroundColor: "#DDCCAE", color: "#67442E" }} onClick={openMaps}>
+            Open Maps
+          </Button>
         </div>
-
-
+      </div>
     </main>
-)
+  );
+};
 
 export default Home;
